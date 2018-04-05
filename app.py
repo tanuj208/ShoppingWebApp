db6541e584b9ec5c0ef25ae72ca46071eabd5f57
mayankmusaddi <mayankmusaddi1997@gmail.com>
2018-04-05 23:16:24 +0530

    YO2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template,session,abort,flash
+from flask import *
 import os
 
 app=Flask(__name__)
@@ -7,13 +7,21 @@ app=Flask(__name__)
 def index():
 	return render_template("homepage.html")
 
-@app.route('/login/', methods=['GET','POST'])
+@app.route('/loginform')
+def loginform():
+	return render_template('login.html')
+
+@app.route('/login', methods=['POST','GET'])
 def login():
-	if request.form['password']=='password' and request.form['username']=='admin':
-		session['logged_in']=True
-	else:
-		flash('wrong password!')
-	return render_template("login.html")
+	if request.method=='POST':
+		if request.form['password']=='password' and request.form['username']=='admin':
+			session['logged_in']=True
+			return redirect(url_for('index'))
+		else:
+			session['logged_in']=False
+			flash('wrong password!')
+			return render_template("login.html")
 
 if __name__=='__main__':
+	app.secret_key=os.urandom(12)
 	app.run(debug=True)
