e8d773d7ebcc37f236b8f676bf9349d2d76d2256
Tanuj Garg <tanuj.garg@students.iiit.ac.in>
2018-04-06 01:00:19 +0530

    yeahhhh

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
 	__tablename__ = "User"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.Sring(255), unique=True)
-    phone = db.Column(db.Integer,unique=True)
-    password = db.Column(db.String(255))
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(255))
+	email = db.Column(db.String(255), unique=True)
+	phone = db.Column(db.Integer,unique=True)
+	password = db.Column(db.String(255))
+
+	def __init__(self,name,email,mobile,password):
+		self.name=name
+		self.email=email
+		self.mobile=mobile
+		self.password=password
 
 @app.route('/', methods=['POST','GET'])
 def index():
@@ -33,9 +39,21 @@ def login():
 			flash('wrong password!')
 			return render_template("login.html")
 
-@app.route('/signup')
+@app.route('/signupform')
 def signupform():
 	return render_template('signup.html')
+
+@app.route('/signup', methods=['POST','GET'])
+def signup():
+	if request.method=='POST':
+		email=str(request.form['email'])
+		name=str(request.form['name'])
+		mobile=str(request.form['mobile'])
+		password=str(request.form['password'])
+		user=User(name,email,mobile,password)
+		db.session.add(user)
+		db.session.commit()
+		return index()
 
 if __name__=='__main__':
 	app.secret_key=os.urandom(12)
